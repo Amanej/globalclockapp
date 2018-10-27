015d34a4fefa0d164af10ef5c5fc99cda4638251
Aman <aman.mender@tegningtil3d.no>
2018-10-27 16:15:20 -0700

    Added built app

---
+++ electron-quick-start-darwin-x64/electron-quick-start.app/Contents/Resources/app/README.md
@@ -0,0 +1,3 @@
+# Global Clock App
+
+I wanted a global clock for my desktop, instead of googling it all the time.
---
+++ electron-quick-start-darwin-x64/electron-quick-start.app/Contents/Resources/app/app.js
@@ -0,0 +1,53 @@
+var clock = new Vue({
+    el: '#clock',
+    data() {
+        return {
+            cities: [
+                {
+                    name: "Oslo",
+                    id: "oslo",
+                    timezone: "Europe/Oslo",
+                    active: false
+                },
+                {
+                    name: "New York",
+                    id: "newyork",
+                    timezone: "America/New_York",
+                    active: false
+                },
+                {
+                    name: "San Francisco",
+                    id: "sanfrancisco",
+                    timezone: "America/Los_Angeles",
+                    active: false
+                }
+            ],
+            time: new Date(),
+            selectedCity: ""
+        }
+    },
+    computed: {
+        selectedTimeZone: function() {
+            console.log(this.selectedCity);
+            var activeCities = this.cities.filter(c => c.name === this.selectedCity);
+            console.log("Active cities ",activeCities);
+            return activeCities[0]
+        }
+    },
+    created () {
+        setInterval(function(timezone) {
+            var d = new Date();
+            var timezone = "Europe/Oslo"; // Default
+            if(clock.selectedTimeZone) {
+                timezone = clock.selectedTimeZone.timezone;
+            }
+            
+            console.log("Selected city ",clock.selectedCity)
+            console.log("Selected timezone ",clock.selectedTimeZone.timezone)
+            var localeSpecificTime = d.toLocaleString('en-US', { timeZone: timezone })
+            //console.log("Today "+localeSpecificTime);;
+            localeSpecificTime.replace(/:\d+ /, ' ');
+            clock.time = localeSpecificTime;
+        }, 1000);
+    }
+});
