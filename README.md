5f480963bad84aff7296c9ba24758274f946cfd4
Aman <aman.mender@tegningtil3d.no>
2018-11-01 02:08:31 -0700

    Improved readme and built the latest version of app

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Global Clock App
 
+Description: An open-source and customizable global clock.
+
+## Screenshot
+
+[a relative link](assets/screenshot.png)
+
+## Background
+
 I wanted a global clock for my desktop, instead of googling it all the time.
+
+## Roadmap
+
+In no particular order.
+
+1. Skins
+2. Larger list of cities (Get and format tsz timezone data)
+3. Style frame
+4. Smoother clock countdown
+5. Force to be always on top (is this possible with Electron)
+6. CircleCi integration
--- electron-quick-start-darwin-x64/electron-quick-start.app/Contents/Resources/app/app.js
+++ electron-quick-start-darwin-x64/electron-quick-start.app/Contents/Resources/app/app.js
@@ -4,34 +4,84 @@ var clock = new Vue({
         return {
             cities: [
                 {
+                    name: "Addis Ababa",
+                    id: "addis",
+                    timezone: "Africa/Addis_Ababa",
+                    added: false,
+                    active: false
+                },
+                {
+                    name: "Asmara",
+                    id: "asmara",
+                    timezone: "Africa/Asmara",
+                    added: true,
+                    active: false
+                },
+                {
+                    name: "Cairo",
+                    id: "cairo",
+                    timezone: "Africa/Cairo",
+                    added: false,
+                    active: false
+                },
+                {
+                    name: "Chicago",
+                    id: "chicago",
+                    timezone: "America/Chicago",
+                    added: false,
+                    active: false
+                },
+                {
+                    name: "Hong Kong",
+                    id: "hongkong",
+                    timezone: "Asia/Hong_Kong",
+                    added: true,
+                    active: false
+                },
+                {
                     name: "Oslo",
                     id: "oslo",
                     timezone: "Europe/Oslo",
+                    added: true,
                     active: false
                 },
                 {
                     name: "New York",
                     id: "newyork",
                     timezone: "America/New_York",
+                    added: true,
                     active: false
                 },
                 {
                     name: "San Francisco",
                     id: "sanfrancisco",
                     timezone: "America/Los_Angeles",
+                    added: true,
                     active: false
                 }
             ],
             time: "Loading",
-            selectedCity: ""
+            selectedCity: "",
+            showCities: false
         }
     },
     computed: {
+        notSelectedCities: function() {
+            return this.cities.filter(c => !c.added);
+        },
+        activeCities: function() {
+            return this.cities.filter(c => c.added);
+        },
         selectedTimeZone: function() {
             console.log(this.selectedCity);
             var activeCities = this.cities.filter(c => c.name === this.selectedCity);
             console.log("Active cities ",activeCities);
             return activeCities[0]
+        }
+    },
+    methods: {
+        setCity: function() {
+            console.log("Set city");
         }
     },
     created () {
